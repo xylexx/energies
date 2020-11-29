ba8985a9027300311ef612fcb8bf0b0b4634d932
AlexisC <alexis.caulier@hotmail.fr>
2020-11-29 23:06:21 +0100

    working app - OK values
    added values

--- energies-app/src/app/app.component.ts
+++ energies-app/src/app/app.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  source1 = "assets/image1.png";
-  source3 = "assets/image3.png";
+  title = 'Moyens de transport et énergies';
+  resetImg = "assets/reset.png"
+
+  // Définition des images pour les transports
+  sourceTransport1 = "assets/avion/avion.png";
+  sourceTransport2 = "assets/bateau/bateau.png";
+  sourceTransport3 = "assets/bus/bus.png";
+  sourceTransport4 = "assets/pieds/pieds.png";
+  sourceTransport5 = "assets/train/train.png";
+  sourceTransport6 = "assets/velo/velo.png";
+  sourceTransport7 = "assets/voiture/voiture.png";
+
+  // Définition des images pour les énergies
+  sourceEnergie1 = "assets/energies/carburant.png";
+  sourceEnergie2 = "assets/energies/charbon.jpg";
+  sourceEnergie3 = "assets/energies/electricite.png";
+  sourceEnergie4 = "assets/energies/nourriture.png";
+  sourceEnergie5 = "assets/energies/soleil.png";
+  sourceEnergie6 = "assets/energies/vent.png";
+
+  // Définition de l'image du résultat
   sourceRes = "";
-  title = 'Energies-app';
+
   selectedTransport = -1;
   selectedEnergie = -1;
 
@@ -19,6 +38,11 @@ export class AppComponent implements OnInit {
 
   }
 
+  reset() {
+    this.selectedTransport = -1;
+    this.selectedEnergie = -1;
+    this.setCombo(this.selectedTransport, this.selectedEnergie);
+  }
 
   setSelectedTransport(id: number) {
     this.selectedTransport = id;
@@ -35,16 +59,209 @@ export class AppComponent implements OnInit {
   }
 
   setCombo(idTransport: number, idEnergie: number){
-    console.log("transport : " + idTransport + " et énergie : " + idEnergie);
-    if(idTransport==1) {
-      if(idEnergie==1){
-        this.sourceRes = "assets/oui.png";
+    // Avion
+    if (idTransport==1) {
+      // Carburant
+      if (idEnergie==1){
+        this.sourceRes = "assets/avion/avion_carburant.png";
       }
-      else if(idEnergie==2){
-        this.sourceRes = "assets/non.png";
+      // Charbon
+      else if (idEnergie==2){
+        this.sourceRes = "assets/avion/avion_charbon.png";
+      }
+      // Electricite
+      else if (idEnergie==3){
+        this.sourceRes = "assets/avion/avion_electricite.png";
+      }
+      // Nourriture
+      else if (idEnergie==4){
+        this.sourceRes = "assets/avion/avion_nourriture.png";
+      }
+      // Soleil
+      else if (idEnergie==5){
+        this.sourceRes = "assets/avion/avion_soleil.png";
+      }
+      // Vent
+      else if (idEnergie==6){
+        this.sourceRes = "assets/avion/avion_vent.png";
       }
       else this.sourceRes="";
     }
+
+    // Bateau
+    else if (idTransport==2){
+      // Carburant
+      if (idEnergie==1){
+        this.sourceRes = "assets/bateau/bateau_carburant.png";
+      }
+      // Charbon
+      else if (idEnergie==2){
+        this.sourceRes = "assets/bateau/bateau_charbon.png";
+      }
+      // Electricite
+      else if (idEnergie==3){
+        this.sourceRes = "assets/bateau/bateau_electrique.png";
+      }
+      // Nourriture
+      else if (idEnergie==4){
+        this.sourceRes = "assets/bateau/bateau_nourriture.png";
+      }
+      // Soleil
+      else if (idEnergie==5){
+        this.sourceRes = "assets/bateau/bateau_soleil.png";
+      }
+      // Vent
+      else if (idEnergie==6){
+        this.sourceRes = "assets/bateau/bateau_vent.png";
+      }
+      else this.sourceRes="";
+    }
+
+    // Bus
+    else if (idTransport==3){
+      // Carburant
+      if (idEnergie==1){
+        this.sourceRes = "assets/bus/bus_carburant.png";
+      }
+      // Charbon
+      else if (idEnergie==2){
+        this.sourceRes = "assets/bus/bus_charbon.png";
+      }
+      // Electricite
+      else if (idEnergie==3){
+        this.sourceRes = "assets/bus/bus_electricite.png";
+      }
+      // Nourriture
+      else if (idEnergie==4){
+        this.sourceRes = "assets/bus/bus_nourriture.png";
+      }
+      // Soleil
+      else if (idEnergie==5){
+        this.sourceRes = "assets/bus/bus_soleil.png";
+      }
+      // Vent
+      else if (idEnergie==6){
+        this.sourceRes = "assets/bus/bus_vent.png";
+      }
+      else this.sourceRes="";
+    }
+
+    // Pieds
+    else if (idTransport==4){
+      // Carburant
+      if (idEnergie==1){
+        this.sourceRes = "assets/pieds/pieds_carburant.png";
+      }
+      // Charbon
+      else if (idEnergie==2){
+        this.sourceRes = "assets/pieds/pieds_charbon.png";
+      }
+      // Electricite
+      else if (idEnergie==3){
+        this.sourceRes = "assets/pieds/pieds_electricite.png";
+      }
+      // Nourriture
+      else if (idEnergie==4){
+        this.sourceRes = "assets/pieds/pieds_nourriture.png";
+      }
+      // Soleil
+      else if (idEnergie==5){
+        this.sourceRes = "assets/pieds/pieds_soleil.png";
+      }
+      // Vent
+      else if (idEnergie==6){
+        this.sourceRes = "assets/pieds/pieds_vent.png";
+      }
+      else this.sourceRes="";
+    }
+
+    // Train
+    else if (idTransport==5){
+      // Carburant
+      if (idEnergie==1){
+        this.sourceRes = "assets/train/train_carburant.png";
+      }
+      // Charbon
+      else if (idEnergie==2){
+        this.sourceRes = "assets/train/train_charbon.png";
+      }
+      // Electricite
+      else if (idEnergie==3){
+        this.sourceRes = "assets/train/train_electricite.png";
+      }
+      // Nourriture
+      else if (idEnergie==4){
+        this.sourceRes = "assets/train/train_nourriture.png";
+      }
+      // Soleil
+      else if (idEnergie==5){
+        this.sourceRes = "assets/train/train_soleil.png";
+      }
+      // Vent
+      else if (idEnergie==6){
+        this.sourceRes = "assets/train/train_vent.png";
+      }
+      else this.sourceRes="";
+    }
+
+    // Velo
+    else if (idTransport==6){
+      // Carburant
+      if (idEnergie==1){
+        this.sourceRes = "assets/velo/velo_carburant.png";
+      }
+      // Charbon
+      else if (idEnergie==2){
+        this.sourceRes = "assets/velo/velo_charbon.png";
+      }
+      // Electricite
+      else if (idEnergie==3){
+        this.sourceRes = "assets/velo/velo_electricite.png";
+      }
+      // Nourriture
+      else if (idEnergie==4){
+        this.sourceRes = "assets/velo/velo_nourriture.png";
+      }
+      // Soleil
+      else if (idEnergie==5){
+        this.sourceRes = "assets/velo/velo_soleil.png";
+      }
+      // Vent
+      else if (idEnergie==6){
+        this.sourceRes = "assets/velo/velo_vent.png";
+      }
+      else this.sourceRes="";
+    }
+
+    // Voiture
+    else if (idTransport==7){
+      // Carburant
+      if (idEnergie==1){
+        this.sourceRes = "assets/voiture/voiture_carburant.png";
+      }
+      // Charbon
+      else if (idEnergie==2){
+        this.sourceRes = "assets/voiture/voiture_charbon.png";
+      }
+      // Electricite
+      else if (idEnergie==3){
+        this.sourceRes = "assets/voiture/voiture_electricite.png";
+      }
+      // Nourriture
+      else if (idEnergie==4){
+        this.sourceRes = "assets/voiture/voiture_nourriture.png";
+      }
+      // Soleil
+      else if (idEnergie==5){
+        this.sourceRes = "assets/voiture/voiture_solaire.png";
+      }
+      // Vent
+      else if (idEnergie==6){
+        this.sourceRes = "assets/voiture/voiture_vent.png";
+      }
+      else this.sourceRes="";
+    }
+
     else this.sourceRes="";
   }
 
